c9391f6482213984f1488ce0d0ae27a69995d543
Jakub Pacler <jakub.pacler@gmail.com>
2024-01-18 22:25:42 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Kolejka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kolejka kolejka = new Kolejka();
+            Console.WriteLine("Program tworzy kolejke oraz daje możliwość dodania do niej elementu, który jest liczbą całkowitą oraz usuwanie elementu");
+
+            int wybor;
+            do
+            {
+                    Console.WriteLine("Wybierz działanie. Wprowadź:");
+                    Console.WriteLine("1 aby dodać element do kolejki");
+                    Console.WriteLine("2 aby usunąć element z kolejki");
+                    Console.WriteLine("3 aby wyjść");
+
+                do
+                {
+                    string input = Console.ReadLine();
+                    if (int.TryParse(input, out wybor))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Błędna wartość. Wprowadź 1 aby dodać, 2 aby usunąć, 3 aby wyjść.");
+                    }
+                } while (true);
+
+                switch (wybor)
+                {
+                    case 1:
+                        kolejka.Add();
+                        break;
+                    case 2:
+                        kolejka.Remove();
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        Console.WriteLine("Błędna wartość. Wprowadź 1 aby dodać, 2 aby usunąć, 3 aby wyjść");
+                        break;
+                }
+
+
+            } while (wybor != 3);
+        }
+
+
+        class Kolejka
+        {
+            private Queue<int> queue;
+
+            public Kolejka()
+            {
+                queue = new Queue<int>();
+            }
+
+            public void Add()
+            {
+                int element;
+                do
+                {
+                    Console.Write("Podaj wartość do wprowadzenia do kolejki. Musi być to liczba całkowita ");
+                    string input = Console.ReadLine();
+
+                    if (int.TryParse(input, out element))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Błędna wartość. Podaj liczbę całkowitą.");
+                    }
+                } while (true);
+
+                queue.Enqueue(element);
+                Console.WriteLine("Dodano do kolejki: " + element);
+            }
+
+            public void Remove()
+            {
+                if (queue.Count == 0)
+                {
+                    Console.WriteLine("Kolejka jest pusta. Usuwanie niemożliwe");
+                    return;
+                }
+                int usunietyElement = queue.Dequeue();
+                Console.WriteLine("Usinięto element: " + usunietyElement);
+            }
+        }
+    }
+}
---
+++ queue/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating a new queue
+            MyQueue myQueue = new MyQueue();
+
+            // Adding elements to the queue
+            myQueue.Add(1);
+            myQueue.Add(2);
+            myQueue.Add(3);
+
+            // Removing and displaying elements from the queue
+            Console.WriteLine("Removed element: " + myQueue.Remove());
+            Console.WriteLine("Removed element: " + myQueue.Remove());
+            Console.WriteLine("Removed element: " + myQueue.Remove());
+
+            // Trying to remove from an empty queue
+            Console.WriteLine("Removed element: " + myQueue.Remove()); // This will print an error message
+
+            Console.ReadLine();
+        }
+    }
+
+    class MyQueue
+    {
+        private Queue<int> queue;
+
+        public MyQueue()
+        {
+            queue = new Queue<int>();
+        }
+
+        public void Add(int element)
+        {
+            queue.Enqueue(element);
+            Console.WriteLine("Added element: " + element);
+        }
+
+        public int Remove()
+        {
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("Queue is empty. Cannot remove.");
+                return -1; // You can choose to throw an exception or handle this case differently based on your needs.
+            }
+
+            int removedElement = queue.Dequeue();
+            Console.WriteLine("Removed element: " + removedElement);
+            return removedElement;
+        }
+    }
+}
